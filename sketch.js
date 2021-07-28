4e8fa838de42821d1be93360ffa282cf9305cd74
akanshapapa <87311597+akanshapapa@users.noreply.github.com>
2021-07-28 16:17:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(DOWN_ARROW))
+{
+  background ("red")
+}
+if(keyIsDown(UP_ARROW))
+{
+  background ("green")
+}
+if(keyIsDown(LEFT_ARROW))
+{
+  background ("blue")
+}
+if(keyIsDown(RIGHT_ARROW))
+{
+  background ("pink")
+}
+
+}
+
+
+
+
